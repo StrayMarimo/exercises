fixed: scaling and comparing old and predicted y values

python/multivariate_regression/multivariate_regression.py
@@ -11,14 +11,10 @@ print(data.head())
 y = data["house price of unit area"].values.reshape(-1, 1)
 X = data.iloc[:, 1:5].values
 
-# scatter plot
-plt.figure(figsize=(10, 6))
 for i in range(X.shape[1]):
-    plt.scatter(X[:, i], y, label=f"{column_names[i+1]}", alpha=0.5)
-plt.title("Scatter Plot of Independent Variables vs. Dependent Variable")
-plt.xlabel("Independent Variables")
-plt.ylabel("House Price of Unit Area")
-plt.legend()
+    column_values = X[:, i]
+    max_value = column_values.max()
+    X[:, i] = column_values / max_value
 
 # apply multivariate regression
 def model(X, y, learning_rate, iterations):
@@ -35,17 +31,31 @@ def model(X, y, learning_rate, iterations):
         cost_list.append(cost)
         if(i%(iterations/10)==0):
             print("Cost after iteration {}: {}".format(i, cost))
-    return theta, cost_list
+    y = np.dot(X, theta)
+    return theta, cost_list, y
 
 # set parameters
-iteration = 100000
-learning_rate = 0.0000000005
+iteration = 10000
+learning_rate = 0.0005
 
-# run the model
-theta, cost_list = model(X, y, learning_rate, iteration)
+# run the modelt
+theta, cost_list, new_y = model(X, y, learning_rate, iteration)
+
+# scatter plot
+plt.figure(figsize=(10, 6))
+for i in range(X.shape[1]):
+    plt.scatter(X[:, i], y, color='black')
+    plt.scatter(X[:, i], new_y,color='blue')
+plt.xlabel('Scaled Feature Values')
+plt.ylabel('Target Variable')
+plt.title('Original vs Predicted Y values')
+plt.legend()
 
 # plot the cost
 rng = np.arange(0, iteration)
 plt.figure(figsize=(10, 6))
+plt.title("Cost over Iterations")
+plt.xlabel("Number of Iterations")
+plt.ylabel("Cost")
 plt.plot(rng, cost_list)
 plt.show()